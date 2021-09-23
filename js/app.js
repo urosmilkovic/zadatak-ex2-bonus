f4c8e9e21f06b39c60283d4ef579f1b6d184dfd0
Uros Milkovic <71762272+urosmilkovic@users.noreply.github.com>
2021-09-23 15:10:04 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.parent').click(function() {
+        $('.response').toggleClass('visible');
+    });
+});
+
+
+$(document).ready(function(){
+    $('.parent').click(function() {
+        $('.tobold').toggleClass('bolded');
+    });
+});
+
+$(document).ready(function(){
+    $('.parent').click(function() {
+        $('.arrowdown').toggleClass('arrowup');
+    });
+});
